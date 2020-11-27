f1d08640d77e82d4274de37151f7e7ac3a11fa14
Diego Pagini <diegopagini777@gmail.com>
2020-11-27 06:36:47 -0300

    comenzando el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionOneComponent } from './section-one/section-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SectionOneComponent
   ],
   imports: [
     BrowserModule,
